7a9702baed3e00e1c0a7b807f1f2cbeb5cc48724
Georgiosss <gniga18@freeuni.edu.ge>
2023-03-30 13:56:21 -0400

    asd

--- problem-1.kt
+++ problem-1.kt
@@ -1,10 +1,11 @@
 import java.util.Scanner
 
-fun main(args: Array<String>) {
+fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
     val reader = Scanner(System.`in`)
     var n:Int = reader.nextInt()
-	for (_ in 1_n) {
+	for (i in 1..n) {
 		var foo:Int = reader.nextInt()
+		foo += 1
 	}
     println("YES")
 }
